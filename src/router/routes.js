c10be95736b366bf4e08f570b88e762aadb5fad9
Luiz Kim <luiz.kim@controleonline.com>
2024-07-28 00:41:34 -0300

    Update

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,14 +1,13 @@
-// export const routes = [
-//   {
-//     path: "/home/",
-//     component: () =>
-//       import("@controleonline/quasar-layout-ui/src/layouts/AdminLayout.vue"),
-//     children: [
-//       {
-//         name: "DashboardPage",
-//         path: "''",
-//         component: () => import("../pages/DashboardPage.vue"),
-//       },
-//     ],
-//   },
-// ];
+export const routes = [
+  {
+    path: "/dashboard/",
+    component: () => import("@controleonline/ui-layout/src/layouts/AdminLayout.vue"),
+    children: [
+      {
+        name: "DashboardIndex",
+        path: "",
+        component: () => import("../pages/DashboardPage.vue"),
+      },
+    ],
+  },
+];
